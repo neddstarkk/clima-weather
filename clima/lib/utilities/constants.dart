4c39b5a71bce9d8410990d6dca563058588fe05b
neddstarkk <nedheesh.hasija@gmail.com>
2019-12-27 14:30:12 +0530

    Boilerplate code

---
+++ clima/lib/utilities/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 30.0,
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
